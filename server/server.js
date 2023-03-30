36e7bcd082540de7a3d953a90314e86a7a2e208a
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-03-30 05:06:14 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,18 +13,29 @@ const courseRoute = require('./app/routes/courseRoute');
 const discussionRoute = require('./app/routes/discussionRoute');
 const notificationRoute = require('./app/routes/notificationRoute');
 const assegnmentRoute = require('./app/routes/assegnmentRoute');
-const submissionRoute = require('./app/routes/submissionRoute');
+// const submissionRoute = require('./app/routes/submissionRoute');
 const commentRoute = require('./app/routes/commentRoute');
 const quizzRoute = require('./app/routes/quizzRoute');
 const scheduleRoute = require('./app/routes/scheduleRoute');
 const lessonRoute = require('./app/routes/lessonRoute');
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+  console.log('A user disconnected');
+  });
+});
+
+app.set('io', io);
 
 app.use(express.json());
 app.use(cookieParser());
 
 
-const dbURI = 'mongodb+srv://saidilena:password@nom.dsjn9yi.mongodb.net/?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://saidilena:lena123@e-khdem.dsjn9yi.mongodb.net/?retryWrites=true&w=majority';
 
 
 app.get("/", (req, res) => {
@@ -34,9 +45,7 @@ mongoose.set({strictQuery: true});
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() =>{
     console.log('DataBase connected...');
-    app.listen(process.env.PORT, ()=> {
-      return console.log(`Server listening on PORT ${process.env.PORT}...`);
-    });
+    app.listen(3000);
   })
   .catch((err) => console.log(err));
 
@@ -46,11 +55,12 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   app.use(discussionRoute);
   app.use(notificationRoute);
   app.use(assegnmentRoute);
-  app.use(submissionRoute);
+  // app.use(submissionRoute);
   app.use(commentRoute);
   app.use(quizzRoute);
   app.use(scheduleRoute);
   app.use(lessonRoute);
+
   
 
 
