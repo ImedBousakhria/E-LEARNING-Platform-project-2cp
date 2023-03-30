6a92dc57208deb0f12896dd1b0e54576c7c6ac4e
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-03-30 05:04:03 +0200

    Update discussionController.js

--- server/app/controllers/discussionController.js
+++ server/app/controllers/discussionController.js
@@ -1,28 +1,31 @@
 const discussion = require('../models/Discussion');
-const Lesson = require('./models/Lesson');
+const Lesson = require('../models/Lesson');
 
   exports.getMessages = async (req, res, next) => {
-    try {
-      const { lessonId } = req.params;
-      const discussion = await Discussion.findOne({ lesson: lessonId }).populate('messages.user', 'name');
-      res.status(200).json(discussion);
-    } catch (error) {
-        res.status(500).json({message: err.message});
-    }
-  };
+  try {
+    const { lessonId } = req.params;
+    const discussion = await Discussion.findOne({ lesson: lessonId }).populate('messages.user', 'name');
+    const io = req.app.get('io');
+    io.emit(`discussion-${lessonId}`, discussion);
+    res.status(200).json(discussion);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
   
   exports.postMessage = async (req, res, next) => {
-    try {
-      const { lessonId } = req.params;
-      const { text, userId } = req.body;
-      const discussion = await Discussion.findOneAndUpdate(
-        { lessonId },
-        { $push: { messages: { text, userId } } },
-        { new: true, upsert: true }
-      );
-      res.status(200).json(discussion);
-    } catch (error) {
-    res.status(500).json({message: err.message});
-
-    }
-  };
+  try {
+    const { lessonId } = req.params;
+    const { text, userId } = req.body;
+    const discussion = await Discussion.findOneAndUpdate(
+      { lesson: lessonId },
+      { $push: { messages: { text, userId } } },
+      { new: true, upsert: true }
+    ).populate('messages.user', 'name');
+    const io = req.app.get('io');
+    io.emit(`discussion-${lessonId}`, discussion); 
+    res.status(200).json(discussion);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+};
