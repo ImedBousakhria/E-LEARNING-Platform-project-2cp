b6f27affef0c61bf60eaf457605d05e22c2f5adb
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-03-30 02:38:35 +0200

    Update discussionController.js

--- server/app/controllers/discussionController.js
+++ server/app/controllers/discussionController.js
@@ -1,1 +1,28 @@
-const discussion = require('../models/discution');
+const discussion = require('../models/Discussion');
+  exports.getMessages = async (req, res, next) => {
+    try {
+      const { lesson } = req.params;
+      const discussions = await Discussion.find({ lesson });
+      res.status(200).json(discussion);
+    } catch (error) {
+        res.status(500).json({message: err.message});
+    }
+  };
+  
+  exports.postMessage = async (req, res, next) => {
+  try {
+    const { lesson } = req.params;
+    const { text } = req.body;
+    const userId = req.user._id; // Assuming that you have implemented authentication and have access to the current user's ID
+
+    const discussion = await Discussion.findOneAndUpdate(
+      { lesson },
+      { $push: { messages: { text, userId } } },
+      { new: true, upsert: true }
+    ).populate('messages.userId', 'name'); // Populate the userId field with the user's name
+
+    res.status(200).json(discussion);
+  } catch (error) {
+    res.status(500).json({message: err.message});
+  }
+};
