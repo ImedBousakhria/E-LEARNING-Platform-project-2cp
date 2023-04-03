2ccf96cda39501bcca25affd70f2669506dea3a5
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-04-03 04:06:06 +0200

    Update Notification.js

--- server/app/models/Notification.js
+++ server/app/models/Notification.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
-      title: {
-      type: String,
-      required: true,
-      },
-      user: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: 'User' 
-      },
-      message: { 
-      type: String,
-      required: true 
-      },
-      read: {
-      type: Boolean,
-      default: false 
-      },
-      createdAt: {
-      type: Date, 
-      default: Date.now 
-      },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  event: {
+    type: String,
+  },
+  eventTitle: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  read: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
 
-  }, {timestamps: true});
-  
-const Notification = mongoose.model("Notification", notificationSchema);
+const Notification = mongoose.model('Notification', notificationSchema);
 
 module.exports = Notification;
