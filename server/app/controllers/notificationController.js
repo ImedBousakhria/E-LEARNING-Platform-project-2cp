1854e0c044f0a841f52a33837cf2376a28d9ae0a
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-04-03 04:05:15 +0200

    Update notificationController.js

--- server/app/controllers/notificationController.js
+++ server/app/controllers/notificationController.js
@@ -1,36 +1,43 @@
-const Notification = require('../models/notification');
+const Notification = require('../models/Notification');
+
+exports.addNotification = async (req, res, next) => {
+  try {
+    const { user, event, eventTitle, message } = req.body;
+    const notification = new Notification({ user, event, eventTitle, message });
+    await notification.save();
+    res.status(201).json(notification);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 exports.getNotifications = async (req, res, next) => {
   try {
-    const userId = req.user._id;
-    const notifications = await Notification.find({ user: userId });
+    const { user } = req;
+    const notifications = await Notification.find({ user }).sort({ createdAt: -1 });
     res.status(200).json(notifications);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-exports.markAllAsRead = async (req, res, next) => {
+exports.markNotificationAsRead = async (req, res, next) => {
   try {
-    const userId = req.user._id;
-    await Notification.updateMany({ user: userId }, { read: true });
-    res.status(200).json({ message: 'All notifications marked as read.' });
+    const { id } = req.params;
+    const notification = await Notification.findByIdAndUpdate(id, { read: true }, { new: true });
+    res.status(200).json(notification);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-exports.markAsRead = async (req, res, next) => {
+exports.markAllNotificationsAsRead = async (req, res, next) => {
   try {
-    const notificationId = req.params.id;
-    const notification = await Notification.findOne({ _id: notificationId });
-    if (!notification) {
-      return res.status(404).json({ message: 'Notification not found.' });
-    }
-    notification.read = true;
-    await notification.save();
-    res.status(200).json(notification);
+    const { user } = req;
+    const notifications = await Notification.updateMany({ user }, { read: true });
+    res.status(200).json({ message: 'All notifications marked as read' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
 };
