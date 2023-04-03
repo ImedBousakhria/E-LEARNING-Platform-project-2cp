f3b65cbbdb22cd0dc0fb4b1dbf86394b19000061
sifoBELAYADI <119885296+sifoBELAYADI@users.noreply.github.com>
2023-04-03 04:01:20 +0200

    Update notificationRoute.js

--- server/app/routes/notificationRoute.js
+++ server/app/routes/notificationRoute.js
@@ -3,7 +3,9 @@ const notificationController = require('../controllers/notificationController');
 
 const router = Router();
 
+router.post('/', notificationController.addNotification);
 router.get('/', notificationController.getAllNotifications);
 router.put('/read', notificationController.markAllAsRead);
 router.put('/read/:id', notificationController.markAsRead);
+
 module.exports = router;
