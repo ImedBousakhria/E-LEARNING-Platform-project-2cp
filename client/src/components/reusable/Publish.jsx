8ee8c9f84e8254424579b237aa5b35847d111768
ImedBousakhria <i_bousakhria@estin.dz>
2023-04-30 20:39:37 +0100

    push

--- client/src/components/reusable/Publish.jsx
+++ client/src/components/reusable/Publish.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import publish from "../../assets/icons/publish.svg";
 
-const Publish = () => {
+const Publish = ({onClick}) => {
   return (
     <div>
       <button
         type="submit"
+        onClick={onClick}
         className="flex items-center gap-2 rounded-[10px] bg-accent px-4 py-2.5 font-semibold text-white"
       >
         <span>Publish</span>
