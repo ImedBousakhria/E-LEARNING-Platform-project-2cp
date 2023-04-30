571644d531f56249ac1aacb1a3c8e205577ba8bc
ImedBousakhria <i_bousakhria@estin.dz>
2023-04-30 15:03:54 +0100

    Debugging

--- client/src/components/reusable/Message.jsx
+++ client/src/components/reusable/Message.jsx
@@ -1,22 +1,6 @@
 import React, { useContext } from 'react'
 import messages from '../../assets/icons/messages.svg' ; 
 
-
-const Message = ({type}) => {
-  var contextProvider ; 
- /*  if(type == "assignment") {
-    contextProvider = IndexElementContext ; 
-  }else {
-    contextProvider =  null; 
-  }
-  const { showDiscussion } = useContext(contextProvider); ; 
-  const handleClick = (e) => {
-    if(showDiscussion[0] == "hidden") {
-      showDiscussion[1]("block")
-    }else {
-      showDiscussion[1]("hidden")
-    }
-  } */
 const Message = ({handleClick}) => {
   return (
     <button onClick={handleClick}>
