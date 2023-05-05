65cfa1bc9b80767b4c1a56d30c99693e94a18304
Lenasaidi <saidilena6@gmail.com>
2023-05-05 22:35:17 +0100

    updtae

--- server/app/middleware/adminMiddleware.js
+++ server/app/middleware/adminMiddleware.js
@@ -5,20 +5,13 @@ module.exports.requireAdmin = (req, res, next)=>{
     //this middleware expects the "user middleware" to be already executed
     
     if(!res.locals.user.isAdmin){
-        // res.status(403).json({message: "Admin permission required"});
+        res.status(403).json({message: "Admin permission required"});
         res.status(303).redirect('/user/login');
     }else{
         next();
     }
 }
 
-// module.exports.requireGerant = (req, res, next) => {
-//   if(res.locals.user.isGerant){
-//     next();
-//   }else{
-//     res.status(303).redirect('/user/login');
-//   }
-// };
 
 module.exports.requireTeacher = (req, res, next) => {
     if(res.locals.user.isTeacher){
--- server/app/middleware/uploadMiddleware.js
+++ server/app/middleware/uploadMiddleware.js
@@ -1,24 +1,24 @@
-// const multer = require('multer');
+const multer = require('multer');
 
-// // Define storage for the files
-// const storage = multer.memoryStorage();
+// Define storage for the files
+const storage = multer.memoryStorage();
 
-// // Filter for allowed file types
-// const fileFilter = function(req, file, callback) {
-//   if (file.mimetype.startsWith('image/') || file.mimetype === 'application/pdf' || file.mimetype.startsWith('video/')) {
-//     callback(null, true);
-//   } else {
-//     callback(new Error('Only images, PDFs, and videos are allowed'));
-//   }
-// };
+// Filter for allowed file types
+const fileFilter = function(req, file, callback) {
+  if (file.mimetype.startsWith('image/') || file.mimetype === 'application/pdf' || file.mimetype.startsWith('video/')) {
+    callback(null, true);
+  } else {
+    callback(new Error('Only images, PDFs, and videos are allowed'));
+  }
+};
 
-// // Initialize multer middleware
-// const upload = multer({ 
-//   storage: storage,
-//   fileFilter: fileFilter,
-//   limits: {
-//     fileSize: 1024 * 1024 * 10 * 100000 // 1000 MB file size limit
-//   }
-// });
+// Initialize multer middleware
+const upload = multer({ 
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 10 * 100000 // 1000 MB file size limit
+  }
+});
 
-// module.exports = upload;
+module.exports = upload;
